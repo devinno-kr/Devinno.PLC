d9d054be659f656c8766914f2043f2f972e82e8e
devinno <devinno@devinno.kr>
2023-07-08 22:08:29 +0900

    -

---
+++ SampleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using Devinno.PLC.Ladder;
+using System;
+
+namespace SampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LadderEngine engine = new LadderEngine();
+            engine.Start();
+            
+            while(true)
+            {
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
